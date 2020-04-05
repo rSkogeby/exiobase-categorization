bab63c1686db15b66067634fd7d6d7db84bf3d6c
Richard Skogeby <richard.skogeby@outlook.com>
2020-04-05 14:22:39 +0100

    Add skeleton interface

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import {
+  Grid,
+  Box,
+  styled,
+  TextField,
+  Button,
+} from '@material-ui/core'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justify="center"
+        style={{ minHeight: '100vh' }}
+      >
+        <Box width='60%'>
+          <p>Company name</p>
+          <p>Description</p>
+        </Box>
+        <Box width='60%'>
+          <Button>
+            Option 1
+          </Button>
+          <Button>
+            Option 1
+          </Button>
+          <Button>
+            Option 1
+          </Button>
+        </Box>
+      </Grid>
+      </>
     </div>
   );
 }
 
-export default App;
+export default App
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders option button link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const buttonElement = getByText(/Option 1/i);
+  expect(buttonElement).toBeInTheDocument();
 });
